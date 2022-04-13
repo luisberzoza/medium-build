ed6c916126928fa595eb0d432529e2d21b35cf38
luisberzoza <73074337+luisberzoza@users.noreply.github.com>
2022-04-13 01:19:17 -0500

    getstaticprops

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,15 +1,78 @@
 import { sanityClient, urlFor } from "../../sanity";
 import Header from "../../components/Header";
+import { GetStaticProps } from "next";
+import { Post } from "../../typings";
 
-function Post() {
-    return <main>
-        <Header />
+interface Props {
+    post: Post;
+}
 
-    </main>;
+function Post({ post }: Props) {
+    console.log(post);
+    
+    return (
+        <main>
+            <Header />
+        </main>
+    );
 }
 
 export default Post;
 
 export const getStaticPaths = async () => {
-    const query = 
+    const query = `*[_type == "post"]{
+        _id,
+        slug {
+         current
+        }
+      }`;
+    const posts = await sanityClient.fetch(query);
+
+    const paths = posts.map((post: Post) => ({
+        params: {
+            slug: post.slug.current
+        }
+    }));
+
+    return {
+        paths,
+        fallback: 'blocking'
+    };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const query = `*[_type == "post" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        title,
+        author-> {
+            name,
+            image
+        },
+        'comments': *[ 
+            _type == "comment" &&
+            post._ref == ^._id &&
+            approved ==true],
+        description,
+        mainImage,
+        slug,
+        body
+    }`
+
+    const post = await sanityClient.fetch(query, {
+        slug: params?.slug,
+    });
+
+    if (!post) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {
+            post,
+        }
+    }
+
 }
