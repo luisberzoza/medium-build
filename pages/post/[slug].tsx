aaee4ad44ce62b5c3b50da6e0f16e4799453d01a
luisberzoza <73074337+luisberzoza@users.noreply.github.com>
2022-04-18 21:51:58 -0500

    added-serializerclass-to-portabletext

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -27,15 +27,30 @@ function Post({ post }: Props) {
                         Blog post by <span className="text-green-600">{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}
                     </p>
                 </div>
-                <div>
+                <div className="mt-10">
                     <PortableText
                     className=""
                     dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
                     projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
                     content={post.body}
+                    serializers={{
+                        h1: (props:any) => (
+                            <h1 className="text-2xl font-bold my-5" {...props}/>
+                        ),
+                        h2: (props:any) => (
+                            <h2 className="text-xl font-bold my-5" {...props}/>
+                        ),
+                        li: ({ children }: any) => (
+                        <li className="ml-4 list-disc">{children}</li>
+                        ),
+                        link: ({ href,children }: any) => (
+                            <a href={href} className="text-blue-500 hover:underline">
+                                {children}
+                            </a>
+                        ),
+                    }}
                     />
                 </div>
-
 
             </article>
         
